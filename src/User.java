046692a51817358202264fa41c66b0e6ca6213ff
Hale <itshalenurgurpinar@gmail.com>
2023-08-04 13:51:15 +0300

    User information

---
+++ src/User.java
@@ -0,0 +1,15 @@
+import java.util.ArrayList;
+import java.util.Date;
+
+public class User {
+
+    private String name;
+    private  String surname;
+    private String email;
+    private String password;
+    private String job;
+    private int age;
+    private Date date;
+
+    ArrayList addressList=new ArrayList();
+}
