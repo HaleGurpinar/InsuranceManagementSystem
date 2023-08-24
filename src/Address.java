06dd765cc331c0576e91ffbf4610da49a96c4671
Hale <itshalenurgurpinar@gmail.com>
2023-08-24 18:39:42 +0300

    add-remove functions

--- src/Address.java
+++ src/Address.java
@@ -57,6 +57,11 @@ class BusinessAddress implements Address{
 
  class AddressManager {
     User user;
-    public static void addAddress(){}
-    public static void removeAddress(){}
+    public static void addAddress(User user,Address address){
+        user.getAddressList().add(address);
+    }
+     public static void removeAddress(User user,Address address){
+         user.getAddressList().remove(address);
+     }
+
 }
