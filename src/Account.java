f001a32c8e883dc05e72b054587b51454a3b7862
Hale <itshalenurgurpinar@gmail.com>
2023-08-26 12:30:11 +0300

    Individual class func.

--- src/Account.java
+++ src/Account.java
@@ -71,3 +71,34 @@ abstract public class Account implements Comparable<Account>{
 
     }
 }
+
+class Individual extends Account{
+    private User user;
+
+    public Individual(){
+        this.setStatus(AuthenticationStatus.FAIL);
+        this.setInsuranceList(new ArrayList<>());
+        setType(0);
+    }
+
+    public Individual(User user) {
+        this.user = user;
+    }
+
+
+    @Override
+    public void addAddress(Address address) {
+        user.getAddressList().add(address);
+    }
+
+    @Override
+    public void removeAddress(Address address) {
+        user.getAddressList().remove(address);
+
+    }
+
+    @Override
+    public void addInsurance(Insurance i) {
+        User.getInsuranceList().add(i);
+    }
+}
